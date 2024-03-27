7f84be9dcd2cc0534978b78aca4c1ad41e1b1d96
JaeKyeong-Kim <106914954+JaeKyeong-Kim@users.noreply.github.com>
2024-03-27 00:43:31 +0000

    histogram

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 x = st.slider('Select a value')
@@ -12,4 +13,9 @@ st.subheader('subheader')
 st.caption('caption')
 st.code('x=2021')
 st.latex(r'''a+ar^1+ar^2+ar^3''')
+
+rand=np.random.normal(1,2,size=20)
+fig, ax= plt.subplots0
+ax.hist(rand, bins=15)
+st.pyplot(fig)
 
