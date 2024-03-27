2edc4dfe846ec84ffdcb5acf08d5ae6ffe54a5b1
JaeKyeong-Kim <106914954+JaeKyeong-Kim@users.noreply.github.com>
2024-03-27 00:41:43 +0000

    ds

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,15 @@
 import streamlit as st
 
+
+
 x = st.slider('Select a value')
 st.write(x, 'is a square ', x * x)
+
+st.title('App title')
+st.header('header - Markdown')
+st.markdown('markdown - Header')
+st.subheader('subheader')
+st.caption('caption')
+st.code('x=2021')
+st.latex(r'''a+ar^1+ar^2+ar^3''')
+
