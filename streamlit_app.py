3f683e48a835b5761fc1f37dc5ced49b94c43a8d
JaeKyeong-Kim <106914954+JaeKyeong-Kim@users.noreply.github.com>
2024-03-27 00:48:37 +0000

    code review

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.code('x=2021')
 st.latex(r'''a+ar^1+ar^2+ar^3''')
 
 rand=np.random.normal(1,2,size=20)
-fig, ax= plt.subplotsO
+fig, ax= plt.subplots()
 ax.hist(rand, bins=15)
 st.pyplot(fig)
 
